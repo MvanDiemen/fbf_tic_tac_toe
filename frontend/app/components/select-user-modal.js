e92a43e5d3d64af5b83b20340ce00cd22d94a686
Michael van Diemen <van.diemen.michael@gmail.com>
2020-06-29 17:55:10 +0200

    Add select user modal
    This modal also adds a 'create user' capability.

--- frontend/app/components/select-user-modal.js
+++ frontend/app/components/select-user-modal.js
@@ -6,10 +6,26 @@ import { action } from '@ember/object';
 export default class SelectUserComponent extends Component {
   @service store;
   @tracked playerList = [];
+  @tracked userName = null;
 
   constructor() {
     super(...arguments);
     this.playerList = this.store.findAll('player');
+  }
+
+  @action
+  addUser () {
+    let newUser = this.store.createRecord('player', { name: this.userName, wins: 0 });
+
+    var onSuccess = function (user) {
+      this.args.select(user.id);
+    };
+
+    var onFail = function (user) {
+      alert(`cannot create ${user.name}`);
+    };
+
+    newUser.save().then(onSuccess, onFail);
   }
 
   @action
