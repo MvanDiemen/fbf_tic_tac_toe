eaed2956ada2db99743f0d82032e68296437fe7c
Michael van Diemen <van.diemen.michael@gmail.com>
2020-06-29 17:53:50 +0200

    Add transformation to ensure edit works

--- frontend/app/models/player.js
+++ frontend/app/models/player.js
@@ -1,6 +1,6 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class PlayerModel extends Model {
-  @attr name;
-  @attr wins;
+  @attr('string') name;
+  @attr('number') wins;
 }
